09b2b9d51f82da7e7f5ed8f10a3a6c87011e1cea
Lloyd <sherewalloyd@gmail.com>
2020-05-22 11:27:14 +0200

    server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,9 +11,32 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      //this.serverStatus = 'Online';
+      this.color = '#66BB6A',
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      //this.serverStatus = 'Offline';
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+  toggleStatus(onlineStatus: boolean){
+    this.serverInput.isOnline = !this.serverInput.isOnline;
+    this.setServerStatus(this.serverInput.isOnline);
+
   }
 
 }
